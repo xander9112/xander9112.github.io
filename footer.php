d5dab1693208e4933d82725037307d1ed1162e1f
Alexandr Furmanchuk <xander@mail.ru>
2016-01-22 15:03:12 +0300

    fix

--- footer.php
+++
@@ -1,6 +0,0 @@
-<script src="https://maps.googleapis.com/maps/api/js"></script>
-
-<script type="text/javascript" src="./site/assets/js/vendor.js"></script>
-<script type="text/javascript" src="./site/assets/js/app.js"></script>
-</body>
-</html>
