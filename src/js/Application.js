f532d50ea28f8144eb8dccadec983548d943673c
AlexZander <xander91@mail.ru>
2018-02-26 22:14:16 +0300

    Add comming soon

--- src/js/Application.js
+++ src/js/Application.js
@@ -11,82 +11,90 @@ import './plugins/jquery.formstyler'
  */
 export default class Application {
   constructor () {
-    this._initComponents()
+    //    this._initComponents()
 
-    $('input, select').styler()
-
-    jQuery.extend(jQuery.validator.messages, {
-      required: 'Поле обязательно для заполнения.',
-      remote: 'Please fix this field.',
-      email: 'Некорректный email.',
-      url: 'Please enter a valid URL.',
-      date: 'Please enter a valid date.',
-      dateISO: 'Please enter a valid date (ISO).',
-      number: 'Please enter a valid number.',
-      digits: 'Please enter only digits.',
-      creditcard: 'Please enter a valid credit card number.',
-      equalTo: 'Пароли не совпадают.',
-      accept: 'Please enter a value with a valid extension.',
-      maxlength: jQuery.validator.format('Please enter no more than {0} characters.'),
-      minlength: jQuery.validator.format('Введите больше чем {0} символов.'),
-      rangelength: jQuery.validator.format('Please enter a value between {0} and {1} characters long.'),
-      range: jQuery.validator.format('Please enter a value between {0} and {1}.'),
-      max: jQuery.validator.format('Please enter a value less than or equal to {0}.'),
-      min: jQuery.validator.format('Please enter a value greater than or equal to {0}.')
+    $(window).on('resize', () => {
+      setTimeout(() => {
+        this.centeredImages()
+      }, 100)
     })
 
-    $('.js-form').each((index, element) => {
-      const formObject = $(element)
+    $(window).trigger('resize')
 
-      formObject.validate({
-        errorClass: 'error',
-        rules: {
-          'email': {
-            required: true,
-            email: true
-          },
-          'name': 'required',
-          'phone': 'required'
-        },
-
-        showErrors: function (errorMap, errorList) {
-          if (this.numberOfInvalids()) {
-            formObject.find('.js-error-content').html('Некорректные данные')
-          } else {
-            formObject.find('.js-error-content').html(' ')
-          }
-          this.defaultShowErrors()
-        },
-
-        highlight: function (element, errorClass) {
-          $(element).parent().addClass('form-group_error')
-          $(element).addClass('error')
-          /* $(element).fadeOut(function() {
-             $(element).fadeIn();
-           });*/
-        },
-
-        unhighlight: function (element, errorClass) {
-          $(element).parent().removeClass('form-group_error')
-          $(element).removeClass('error')
-          /* $(element).fadeOut(function() {
-             $(element).fadeIn();
-           });*/
-        },
-
-        submitHandler: (form, event) => {
-          event.preventDefault()
-
-          $(form).find('.js-error-content').html(' ')
-
-          return false
-        }
-      })
-    })
-
-    $('.js-tabs').each((index, element) => {
-      new Tabs($(element))
-    })
+    //    $('input, select').styler()
+    //
+    //    jQuery.extend(jQuery.validator.messages, {
+    //      required: 'Поле обязательно для заполнения.',
+    //      remote: 'Please fix this field.',
+    //      email: 'Некорректный email.',
+    //      url: 'Please enter a valid URL.',
+    //      date: 'Please enter a valid date.',
+    //      dateISO: 'Please enter a valid date (ISO).',
+    //      number: 'Please enter a valid number.',
+    //      digits: 'Please enter only digits.',
+    //      creditcard: 'Please enter a valid credit card number.',
+    //      equalTo: 'Пароли не совпадают.',
+    //      accept: 'Please enter a value with a valid extension.',
+    //      maxlength: jQuery.validator.format('Please enter no more than {0} characters.'),
+    //      minlength: jQuery.validator.format('Введите больше чем {0} символов.'),
+    //      rangelength: jQuery.validator.format('Please enter a value between {0} and {1} characters long.'),
+    //      range: jQuery.validator.format('Please enter a value between {0} and {1}.'),
+    //      max: jQuery.validator.format('Please enter a value less than or equal to {0}.'),
+    //      min: jQuery.validator.format('Please enter a value greater than or equal to {0}.')
+    //    })
+    //
+    //    $('.js-form').each((index, element) => {
+    //      const formObject = $(element)
+    //
+    //      formObject.validate({
+    //        errorClass: 'error',
+    //        rules: {
+    //          'email': {
+    //            required: true,
+    //            email: true
+    //          },
+    //          'name': 'required',
+    //          'phone': 'required'
+    //        },
+    //
+    //        showErrors: function (errorMap, errorList) {
+    //          if (this.numberOfInvalids()) {
+    //            formObject.find('.js-error-content').html('Некорректные данные')
+    //          } else {
+    //            formObject.find('.js-error-content').html(' ')
+    //          }
+    //          this.defaultShowErrors()
+    //        },
+    //
+    //        highlight: function (element, errorClass) {
+    //          $(element).parent().addClass('form-group_error')
+    //          $(element).addClass('error')
+    //          /* $(element).fadeOut(function() {
+    //             $(element).fadeIn();
+    //           });*/
+    //        },
+    //
+    //        unhighlight: function (element, errorClass) {
+    //          $(element).parent().removeClass('form-group_error')
+    //          $(element).removeClass('error')
+    //          /* $(element).fadeOut(function() {
+    //             $(element).fadeIn();
+    //           });*/
+    //        },
+    //
+    //        submitHandler: (form, event) => {
+    //          event.preventDefault()
+    //
+    //          $(form).find('.js-error-content').html(' ')
+    //
+    //          return false
+    //        }
+    //      })
+    //    })
+    //
+    //    $('.js-tabs').each((index, element) => {
+    //      new Tabs($(element))
+    //    })
   }
 
   _initComponents () {
@@ -148,12 +156,12 @@ export default class Application {
       $('.js-center-image').each((index, element) => {
         const image = $(element)
         const parent = image.parent()
-        const marginLeft = image.width() > parent.width() ? (image.width() - parent.width()) / 2 : 0
+        const marginLeft = (parent.width() - image.width()) / 2
 
         image.css({
           width: 'auto',
           float: 'left',
-          marginLeft: `-${marginLeft}px`
+          marginLeft: `${marginLeft}px`
         })
       })
     }
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,3 @@
-import 'bootstrap'
 import Application from './Application'
 
 const $$ = $$ || {}
